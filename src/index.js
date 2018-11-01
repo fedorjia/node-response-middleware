3a0a1903704fcec9f76c8f6e21124b6e40263443
fedor <332762488@qq.com>
2018-11-01 10:31:12 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+const axios = require('axios')
+
 const status = require('./response-status')
 const ValidateError = require('./validate-error')
 const responseStatus = require('./response-status')
 // const logErr = require('./logerr')
 // const isDev = process.env.NODE_ENV === 'development'
 
+const filterBody = function(body = {}) {
+	const rs = {}
+	for (let k in body) {
+		if (JSON.stringify(body[k]).length < 64) { // exclude value which very long
+			rs[k] = body[k]
+		}
+	}
+	return rs
+}
+
 /**
  * setup exception middlewares
+ *
+ *	isUseLog: 是否使用logger,
+ *	logUrl:   logger的URL
+ *	extra: {
+ *		app: 业务名
+ *	}
  */
-exports.setupExceptionMiddlewares = function(app, appname = '') {
+exports.setupExceptionMiddlewares = function(app, {isUseLog = false, logUrl = null, extra = {}}) {
 	/**
 	 * error handling
 	 */
@@ -15,13 +33,33 @@ exports.setupExceptionMiddlewares = function(app, appname = '') {
 		if (typeof err === "string") {
 			res.json({status: status.SERVICE_ERROR, body: err})
 		} else {
+			if (err.hasOwnProperty('status') && err.hasOwnProperty('body')) {
+				return res.json(err)
+			}
+
 			console.log(err)
 			res.json({status: status.INTERNAL_ERROR, body: 'internal error!'})
-			// if (!isDev) { // log error
-			// 	logErr(appname, req.originalUrl, null, err.message)
-			// }
+
+			// log error
+			if (isUseLog && logUrl) {
+				axios({
+					method: 'POST',
+					url: logUrl,
+					data: {
+						url: req.originalUrl,
+						method: req.method.toLowerCase(),
+						content: JSON.stringify({
+							query: req.query,
+							body: filterBody(req.body)
+						}),
+						message: err.message,
+						...extra
+					}
+				})
+			}
 		}
 	})
+
 	app.use((req, res, ignore) => {
 		res.json({status: status.REQUEST_NOT_FOUND, body: 'request not found!'})
 	})
@@ -29,24 +67,57 @@ exports.setupExceptionMiddlewares = function(app, appname = '') {
 
 /**
  * response middleware
+ *
+ *  isUseLog: 是否使用log,
+ *	logUrl:   log请求的URL,
+ *	extra: {
+ *		app: 业务名,
+ *		created_type: 操作人类型，默认1
+ *	}
  */
-exports.responseMiddleware = function (req, res, next) {
+exports.responseMiddleware = function({isUseLog = false, logUrl = null, extra = {}}) {
+	return async (req, res, next) => {
 
-	res.success = (data) => {
-		return res.json({ status: status.SUCCESS, body: data })
-	}
+		const method = req.method.toLowerCase()
+		const user = res.locals.user
 
-	res.failure = (err) => {
-		if(typeof err === "string") {
-			res.json({ status: status.SERVICE_ERROR, body:  err.toString() })
-		} else if(err instanceof ValidateError) {
-			res.json({ status: status.REQUEST_ERROR, body:  err.errors[0].msg })
-		} else {
-			res.json({ status: status.SERVICE_ERROR, body: err.message })
+		res.success = (data) => {
+
+			// log action
+			if (isUseLog && logUrl) {
+				if (method !== 'get' && user) {
+					axios({
+						method: 'POST',
+						url: logUrl,
+						data: {
+							url: req.originalUrl,
+							method: method,
+							content: JSON.stringify({
+								query: req.query,
+								body: filterBody(req.body)
+							}),
+							createdBy: user.id,
+							...extra
+						}
+					})
+				}
+			}
+
+			return res.json({ status: status.SUCCESS, body: data })
 		}
-	}
 
-	return next()
+		res.failure = (err) => {
+			if(typeof err === "string") {
+				res.json({ status: status.SERVICE_ERROR, body:  err.toString() })
+			} else if(err instanceof ValidateError) {
+				res.json({ status: status.REQUEST_ERROR, body:  err.errors[0].msg })
+			} else {
+				res.json({ status: status.SERVICE_ERROR, body: err.message })
+			}
+		}
+
+		return next()
+	}
 }
 
 /**
--- src/response-status.js
+++ src/response-status.js
@@ -1,7 +1,8 @@
 module.exports = {
-	SUCCESS: 22000, 			// 成功
-	SERVICE_ERROR: 22001, 		// 业务逻辑异常
-	REQUEST_ERROR: 22002, 		// 请求参数错误
-	REQUEST_NOT_FOUND: 22003, 	// 请求未找到
-	INTERNAL_ERROR: 22004 		// 内部错误
+	SUCCESS: 200, 				// 成功
+	SERVICE_ERROR: 300, 		// 业务逻辑异常
+	REQUEST_ERROR: 400, 		// 请求参数错误
+	REQUEST_NOT_FOUND: 404, 	// 请求未找到
+	INTERNAL_ERROR: 500, 		// 内部错误
+	NEED_LOGIN: 1001			// 需要登录
 }
