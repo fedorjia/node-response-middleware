babda7e1f3d50d628623b029c2373e25261f31bf
fedor <332762488@qq.com>
2018-12-24 08:58:42 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
-const axios = require('axios')
-
-const status = require('./response-status')
+let status = require('./response-status')
 const ValidateError = require('./validate-error')
-const responseStatus = require('./response-status')
-// const logErr = require('./logerr')
-// const isDev = process.env.NODE_ENV === 'development'
 
-const filterBody = function(body = {}) {
-	const rs = {}
-	for (let k in body) {
-		if (JSON.stringify(body[k]).length < 64) { // exclude value which very long
-			rs[k] = body[k]
-		}
-	}
-	return rs
+/**
+ * response status
+ */
+exports.responseStatus = status
+
+/**
+ * reset status
+ */
+exports.resetStatus = function(newStatus = {}) {
+	status = Object.assign(status, newStatus)
 }
 
 /**
  * setup exception middlewares
- *
- *	isUseLog: 是否使用logger,
- *	logUrl:   logger的URL
- *	extra: {
- *		app: 业务名
- *	}
  */
-exports.setupExceptionMiddlewares = function(app, {isUseLog = false, logUrl = null, extra = {}}) {
-	/**
-	 * error handling
-	 */
+exports.setupExceptionMiddlewares = function(app) {
 	app.use((err, req, res, ignore) => {
 		if (typeof err === "string") {
 			res.json({status: status.SERVICE_ERROR, body: null, message: err})
@@ -41,24 +29,6 @@ exports.setupExceptionMiddlewares = function(app, {isUseLog = false, logUrl = nu
 
 			console.log(err)
 			res.json({status: status.INTERNAL_ERROR, body: null, message: 'internal error!'})
-
-			// log error
-			if (isUseLog && logUrl) {
-				axios({
-					method: 'POST',
-					url: logUrl,
-					data: {
-						url: req.originalUrl,
-						method: req.method.toLowerCase(),
-						content: JSON.stringify({
-							query: req.query,
-							body: filterBody(req.body)
-						}),
-						message: err.message,
-						...extra
-					}
-				})
-			}
 		}
 	})
 
@@ -69,60 +39,21 @@ exports.setupExceptionMiddlewares = function(app, {isUseLog = false, logUrl = nu
 
 /**
  * response middleware
- *
- *  isUseLog: 是否使用log,
- *	logUrl:   log请求的URL,
- *	extra: {
- *		app: 业务名,
- *		created_type: 操作人类型，默认1
- *	}
  */
-exports.responseMiddleware = function({isUseLog = false, logUrl = null, extra = {}}) {
-	return async (req, res, next) => {
-
-		const method = req.method.toLowerCase()
-		const user = res.locals.user
-
-		res.success = (data) => {
-
-			// log action
-			if (isUseLog && logUrl) {
-				if (method !== 'get' && user) {
-					axios({
-						method: 'POST',
-						url: logUrl,
-						data: {
-							url: req.originalUrl,
-							method: method,
-							content: JSON.stringify({
-								query: req.query,
-								body: filterBody(req.body)
-							}),
-							createdBy: user.id,
-							...extra
-						}
-					})
-				}
-			}
-
-			return res.json({ status: status.SUCCESS, body: data, message: '' })
-		}
-
-		res.failure = (err) => {
-			if(typeof err === "string") {
-				res.json({ status: status.SERVICE_ERROR, body: null, message:  err.toString() })
-			} else if(err instanceof ValidateError) {
-				res.json({ status: status.REQUEST_ERROR, body: null, message:  err.errors[0].msg })
-			} else {
-				res.json({ status: status.SERVICE_ERROR, body: null, message: err.message })
-			}
-		}
-
-		return next()
+exports.responseMiddleware = async (req, res, next) => {
+	res.success = (data) => {
+		return res.json({ status: status.SUCCESS, body: data, message: '' })
 	}
-}
 
-/**
- * response status
- */
-exports.responseStatus = responseStatus
+	res.failure = (err) => {
+		if(typeof err === "string") {
+			res.json({ status: status.SERVICE_ERROR, body: null, message:  err.toString() })
+		} else if(err instanceof ValidateError) {
+			res.json({ status: status.REQUEST_ERROR, body: null, message:  err.errors[0].msg })
+		} else {
+			res.json({ status: status.SERVICE_ERROR, body: null, message: err.message })
+		}
+	}
+
+	return next()
+}
